c8e884a7e9ffb67a65bb88d21241bec913f30907
dadynapoleao <dadynapoleao@gmail.com>
2026-03-05 15:42:52 +0000

    Create she.js

---
+++ js/she.js
@@ -0,0 +1,25 @@
+[
+    {
+        "Name": "Payton Preslee",
+        "Born": "24/2/1994",
+        "Years active": "2019",
+        "Birthplace": "United States",
+        "Ethnicity": "Caucasian",
+        "Hair color": "Black",
+        "Eye color": "Blue",
+        "Height": "160",
+        "Weight": "57",
+        "Body type": "Average",
+        "Measurements": "36F-24-36",
+        "Boobs": "36",
+        "Hips": "24",
+        "Ass": "36",
+        "Total": 48,
+        "Tattoos": "Yes",
+        "Piercings": "Yes"
+    }
+
+
+
+
+]
